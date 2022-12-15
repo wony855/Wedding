b8fc1115329ceba8a4ef85c42a85bc28b8172cd0
wony <wony@gameplete.net>
2022-12-16 00:16:57 +0900

    notice

--- src/app.vue
+++ src/app.vue
@@ -3,7 +3,9 @@
 </template>
 
 <script>
-import Home from '@/views/invitation.vue'
+//import Home from '@/views/invitation.vue'
+//import Home from '@/views/notice.vue'
+import Home from '@/views/simple.vue'
 export default {
   components: { Home }
 }
--- src/views/simple.vue
+++ src/views/simple.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <header-part></header-part>
-    <account-part></account-part>
     <contact-part></contact-part>
     <footer-part></footer-part>
   </div>
@@ -9,11 +8,10 @@
 
 <script>
 import HeaderPart from "@/components/header2.vue";
-import AccountPart from "@/components/account.vue";
 import ContactPart from "@/components/contact.vue";
 import FooterPart from '@/components/footer.vue';
 export default {
-  components: { HeaderPart, AccountPart, ContactPart, FooterPart }
+  components: { HeaderPart, ContactPart, FooterPart }
 };
 </script>
 
